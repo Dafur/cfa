8c28a6c21a440acd7601603f20d218ea822fa646
Nikolay Morozov <nmorozov@cloudfactory.ru>
2025-01-14 11:53:55 +0300

    Урок 12, задание 3

---
+++ SQL/table/lesson_12_task_3.sql
@@ -0,0 +1,15 @@
+﻿set ansi_nulls, ansi_padding, quoted_identifier on;
+go
+
+if object_id('dbo.students') is null
+    create table dbo.students (
+        student_id          int           not null identity(1, 1),
+        student_first_name  nvarchar(100) not null,
+        student_last_name   nvarchar(100) not null,
+       constraint student_id_pk primary key clustered (student_id) with (fillfactor = 100, pad_index = off, statistics_norecompute = off, allow_row_locks = on)
+    )
+go
+
+if columnproperty(object_id('dbo.students'), 'exam_score', 'ColumnId') is null
+  alter table dbo.students add exam_score int null
+go
