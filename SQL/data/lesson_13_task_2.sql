702e499fb8ca35ff6a1b7c08cee9ac66325fc66b
Nikolay Morozov <nmorozov@cloudfactory.ru>
2025-01-20 12:12:17 +0300

    Урок 13, задание 2

---
+++ SQL/data/lesson_13_task_2.sql
@@ -0,0 +1,25 @@
+﻿set identity_insert dbo.directions on
+go
+
+merge dbo.directions as trg
+using (
+  select top 0 direction_id, direction_name, min_exam_score
+  from dbo.directions
+  union all
+  select 1, 'История', 160
+  union all
+  select 2, 'Право', 180
+  union all
+  select 3, 'Физика', 200
+) as src on (trg.direction_id = src.direction_id)
+when matched then
+  update set
+    direction_name  = src.direction_name,
+    min_exam_score   = src.min_exam_score
+when not matched then
+  insert (direction_id, direction_name, min_exam_score)
+  values (src.direction_id, src.direction_name, src.min_exam_score);
+go
+
+set identity_insert dbo.directions off
+go
