0791aedf4cdc513a6de399c0bfc0db11d05c32c9
Nikolay Morozov <nmorozov@cloudfactory.ru>
2025-01-24 11:25:06 +0300

    Урок 13, задание 3

---
+++ SQL/data/lesson_13_task_3.sql
@@ -0,0 +1,6 @@
+﻿update s set direction_id = d.direction_id
+from dbo.students s
+inner join dbo.directions d on d.direction_name = s.direction
+where s.direction_id != d.direction_id
+  or s.direction_id is null
+go
