2bf27008d1907e1f97d341844c0a9c71e13f9eb4
Nikolay Morozov <nmorozov@cloudfactory.ru>
2025-01-28 10:43:54 +0300

    Урок 14, задание 1

---
+++ SQL/query/lesson_14_task_1.sql
@@ -0,0 +1,10 @@
+﻿
+select s.*
+  , d.min_exam_score
+  , [Зачислен] = case
+            when s.exam_score >= d.min_exam_score then 'Да'
+            when s.exam_score < d.min_exam_score then 'Нет'
+            else null
+            end
+from dbo.students s
+left join dbo.directions d on d.direction_id = s.direction_id
