eb7b982aa918e9bdfdaa4c63015ba9de4e717c46
Nikolay Morozov <nmorozov@cloudfactory.ru>
2025-01-28 16:07:21 +0300

    Урок 14, задание 2

---
+++ SQL/query/lesson_14_task_2.sql
@@ -0,0 +1,19 @@
+﻿
+select stuff(
+  cast(
+      (
+      select [text()] = ', ' + convert(varchar(max), student_last_name)
+      from
+      (
+        select student_last_name
+        from dbo.students s
+        join dbo.directions d on d.direction_id = s.direction_id
+        where d.direction_id = 1 --История
+          and s.exam_score >= d.min_exam_score
+      ) S
+      for xml path(''), type
+      )
+    as varchar(max)
+    )
+  , 1, 2 ,''
+  ) as Students
